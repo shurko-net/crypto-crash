0a4e59e1cd2a5af1eb137d438b1aed7c325081d5
Yaroslav <ytube.noobtv@gmail.com>
2025-06-08 02:18:54 +0300

    Eit header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <header className="fixed lg:relative top-0 z-10  py-3.5 w-full ">
       <div className="absolute inset-x-0 top-0 pointer-events-none -bottom-3 max-lg:bg-[image:linear-gradient(#1c123f_20%,#1c123fa1_70%,#1c123f1f_90%,transparent)] -z-1"></div>
-      <div className="container flex">
+      <div className="container flex items-center justify-end">
         <div className="navbar-start w-auto lg:w-1/2">
           <Link href="/" passHref className="inline-flex items-center gap-2  shrink-0 ">
             <LogoImage />
